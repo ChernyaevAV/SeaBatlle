39cc27c7d9daaa3601da3a9a7131fe0d8d1c1237
chernyaevav <chernyaevav@gmail.com>
2023-07-15 11:51:31 +0300

    init config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+block_size = 50
+left_margin = 100
+upper_margin = 80
+
+size = (left_margin+30*block_size, upper_margin+15*block_size)
+font_size = int(block_size / 1.5)
