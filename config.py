5289d77bd822d04f91ab3db4ec3052ee347eabbd
chernyaevav <chernyaevav@gmail.com>
2023-07-15 12:09:56 +0300

    запуск окна с сеткой

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
-block_size = 50
-left_margin = 100
-upper_margin = 80
+block_size = 30
+left_margin = 40
+upper_margin = 50
 
 size = (left_margin+30*block_size, upper_margin+15*block_size)
 font_size = int(block_size / 1.5)
